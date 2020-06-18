604901af7a29590ff811af23d970b1b3c923f607
champ8644 <varis_champ@hotmail.com>
2020-06-19 00:41:42 +0700

    save

--- functions/afterHooks.js
+++ functions/afterHooks.js
@@ -2,7 +2,6 @@ function afterHooks() {
   const execSync = require('child_process').execSync;
   execSync('yarn patch', { encoding: 'utf-8' });
   execSync('shutdown -s -t 60', { encoding: 'utf-8' });
-  console.log('going to shutdown');
 }
 
 afterHooks();
