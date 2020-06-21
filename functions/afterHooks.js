1763dbfbd7677abc10433c6e6bf2ad49e78f17d6
champ8644 <varis_champ@hotmail.com>
2020-06-21 21:52:25 +0700

    fix hooks

--- functions/afterHooks.js
+++ functions/afterHooks.js
@@ -2,6 +2,8 @@ module.exports = {
   default: function afterHooks() {
     const execSync = require('child_process').execSync;
     // execSync('yarn patch', { encoding: 'utf-8' });
-    execSync('shutdown -s -t 300', { encoding: 'utf-8' });
+    console.log('process.env.SHUTDOWN: ', process.env.SHUTDOWN);
+    if (process.env.SHUTDOWN)
+      execSync('shutdown -s -t 300', { encoding: 'utf-8' });
   }
 };
