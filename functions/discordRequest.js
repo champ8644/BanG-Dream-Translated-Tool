706193b0b18e87f252f33c3e86d79a241c4f881e
champ8644 <varis_champ@hotmail.com>
2020-06-27 14:21:05 +0700

    finish discordrequest

--- functions/discordRequest.js
+++ functions/discordRequest.js
@@ -1,24 +1,51 @@
 /* eslint-disable */
 
-const axios = require('axios');
+const { join } = require('path');
+const { readFileSync } = require('fs');
+const { rootPath } = require('electron-root-path');
 
-axios({
-  method: 'post',
-  url:
-    'https://discordapp.com/api/webhooks/726136519710081145/9yx9OHM1dGswKFqNJGsSQ5kafTREERxIZlaSw_YIsS3wNg_gCgSdOt1ulHRs2ehUhBWD',
-  data: {
-    embeds: [
-      {
-        title: 'Release notes from BanG-Dream-Translated-Tool',
-        description: 'v1.15.15',
-        url:
-          'https://github.com/champ8644/BanG-Dream-Translated-Tool/releases/download/v1.15.15/BanG-Dream-Translated-Tool-Setup-1.15.15.exe',
-        thumbnail: {
-          url:
-            'https://raw.githubusercontent.com/champ8644/BanG-Dream-Translated-Tool/master/build/icons/128x128.png'
-        }
-        // timestamp: 'June 27, 2020 at 11:04AM'
+let pkginfo = {};
+
+// eslint-disable-next-line no-undef
+if (typeof PKG_INFO !== 'undefined' && PKG_INFO !== null) {
+  // eslint-disable-next-line no-undef
+  pkginfo = PKG_INFO;
+} else {
+  pkginfo = JSON.parse(
+    readFileSync(join(rootPath, 'package.json'), { encoding: 'utf8' })
+  );
+}
+
+const { version } = pkginfo;
+
+const APP_VERSION = version;
+
+module.exports = {
+  default: function discordRequest() {
+    require('axios')({
+      method: 'post',
+      url:
+        'https://discordapp.com/api/webhooks/726136519710081145/9yx9OHM1dGswKFqNJGsSQ5kafTREERxIZlaSw_YIsS3wNg_gCgSdOt1ulHRs2ehUhBWD',
+      data: {
+        embeds: [
+          {
+            title: 'New release from BanG Dream Translated Tool',
+            description:
+              'BanG-Dream-Translated-Tool-Setup-' + APP_VERSION + '.exe',
+            url:
+              'https://github.com/champ8644/BanG-Dream-Translated-Tool/releases/download/v' +
+              APP_VERSION +
+              '/BanG-Dream-Translated-Tool-Setup-' +
+              APP_VERSION +
+              '.exe',
+            thumbnail: {
+              url:
+                'https://raw.githubusercontent.com/champ8644/BanG-Dream-Translated-Tool/master/build/icons/128x128.png'
+            },
+            timestamp: new Date().toISOString()
+          }
+        ]
       }
-    ]
+    });
   }
-});
+};
