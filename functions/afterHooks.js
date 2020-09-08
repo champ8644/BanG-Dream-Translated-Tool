963b456f5b8c53fb489b7c0060ab79deddddafbb
Champ <varis_champ@hotmail.com>
2020-09-09 02:23:28 +0700

    save

--- functions/afterHooks.js
+++ functions/afterHooks.js
@@ -1,7 +1,7 @@
 module.exports = {
   default: function afterHooks() {
-    // const execSync = require('child_process').execSync;
-    // require('./discordRequest').default();
+    const execSync = require('child_process').execSync;
+    require('./discordRequest').default();
     // execSync('yarn patch', { encoding: 'utf-8' });
     // console.log('process.env.SHUTDOWN: ', process.env.SHUTDOWN);
     // execSync('shutdown -s -t 300', { encoding: 'utf-8' });
